ba7ec3e3093d3ecd4aa1d54de8657f5ade6282ab
Pedro Henrique <pedro97henrique@gmail.com>
2019-10-31 22:57:16 -0300

    add colors

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -7,12 +7,27 @@ class MyHome extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('PokeGraph'),
-        backgroundColor: Colors.cyan,
+        backgroundColor: Colors.red[600],
+        centerTitle: true,
       ),
       drawer: Drawer(),
       body: Container(
         color: Colors.white,
+        child: Center(
+          child: Text(
+            'Hellooo mundo',
+            style: TextStyle(
+              fontSize: 30.0,
+              fontWeight: FontWeight.bold,
+            ),
+            )
+          ),
       ),
+      floatingActionButton: FloatingActionButton(
+        child: Text('ClickMe'),
+        onPressed: () => print('Teste'),
+        backgroundColor: Colors.red[600],
+      ) ,
     );
   }
 }
